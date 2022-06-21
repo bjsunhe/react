f8c5cee97b20a32882459f0ea58a97473dfb8ecc
bjsunhe <bjsunhe@sina.com>
2022-06-21 10:09:36 +0800

    state

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,31 +1,22 @@
 import React, {useState} from 'react'
 
-const Accordion =({items})=>{
+const Accordion=({items})=>{
     const [activeIndex,setActiveIndex]=useState(null)
-    const onTitleClick=(index)=>{
-        setActiveIndex(index)
-    }
 
-    const renderedItems=items.map(
-        (item,index)=>{
-           const active= activeIndex===index?'active':''
-           return (<React.Fragment key={item.title}>
-                <div className={`title ${active}`} onClick={()=>onTitleClick(index)} >
-                    <i className="dropdown icon"></i>
-                    {item.title}
-                </div>
-                <div className={`content ${active}`}>
-                    <p>{item.content}</p>    
-                </div>
-            </React.Fragment>)
-        }
-    )
+    const onTitleClick=(index)=>setActiveIndex(index)
+    const renderedItems=items.map((item,index)=>{
+        index===activeIndex?'active':''
+        
+        return (
+            <React.Fragment key={item.title}>
+                    <h1 className={`title ${active}`} onClick={()=>onTitleClick(index)}>{item.title}</h1>
+                    <p className={`content ${active}`}>{item.content}</p>
 
-
-
-
+            </React.Fragment>
+        )
+    })
     return (
-        <div className="ui styled accordion">
+        <div>
             {renderedItems}
         </div>
     )
