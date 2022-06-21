7acd057f0e6631f9023a520d2e6c31b20154923b
bjsunhe <bjsunhe@sina.com>
2022-06-21 17:00:59 +0800

    promise

---
+++ es6/promise.js
@@ -0,0 +1,55 @@
+const STATE={
+    FULFILLED:'fulfilled',
+    REJECTED:'rejected',
+    PENDING:'pending'
+}
+
+
+class MyPromise{
+    constructor(){
+
+    }
+
+    #runCallbacks(){
+
+    }
+
+    #onSuccess(){
+
+    }
+    #onFail(){
+
+    }
+    then(){
+
+    }
+    catch(){
+
+    }
+    finally(){
+
+    }
+    static resolve(){
+
+    }
+    static reject(){
+
+    }
+    static all(){
+
+    }
+    static allSettled(){
+
+    }
+    static race(){
+
+    }
+    static any(){
+
+    }
+
+}
+
+class UncaughtPromiseError extends Error{
+
+}
