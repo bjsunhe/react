6dc376dae0be56d9df2e11af56f6f64f57927d39
bjsunhe <bjsunhe@sina.com>
2022-06-21 17:21:25 +0800

    dropdown

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,7 +1,10 @@
-import React, {useEffect, useState, useRef} from 'react'
-const Dropdown=({options,selected, onSelectedChange})=>{
-    const [open, setOpen]=useState(false)
+import React, {useState, useEffect, useRef} from 'react'
+
+const Dropdown=({options,selected,onSelectedChange})=>{
+
+    const [open,setOpen]=useState(false)
     const ref=useRef()
+
     useEffect(()=>{
         const onBodyClick=(event)=>{
             if(ref.current.contains(event.target)){
@@ -9,32 +12,27 @@ const Dropdown=({options,selected, onSelectedChange})=>{
             }
             setOpen(false)
         }
-
         document.body.addEventListener('click',onBodyClick)
         return ()=>{
             document.body.removeEventListener('click',onBodyClick)
         }
     },[])
+
     const renderedOptions=options.map(option=>{
         if(option.value===selected.value){
             return null
         }
+
         return (
-            <div
-                key={option.value}
-                onClick={()=>onSelectedChange(option)}
-            >
+            <div onClick={()=>onSelectedChange(option)}>
                 {option.label}
             </div>
         )
     })
-
     return (
         <div ref={ref}>
-            <div onClick={()=>{setOpen(!open)}} className={`ui selection dropdown ${open?'visible active':''}`}>
-                <div>{selected.label}</div>
-                <div className={`menu ${open? 'visible transition':''}`}>{renderedOptions}</div>
-            </div>
+            <div onClick={()=>setOpen(!open)}>{selected.label}</div>
+            {renderedOptions}
         </div>
     )
 }
