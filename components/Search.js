3bae5960af42f64e9ff07039f3bb0bb7a1826e21
bjsunhe <bjsunhe@sina.com>
2022-06-21 11:30:06 +0800

    search

--- components/Search.js
+++ components/Search.js
@@ -2,9 +2,8 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const Search=()=>{
-
-    const [term,setTerm]=useState('programming')
-    const [debouncedTerm, setDebouncedTerm]=useState(term)
+    const [term, setTerm]=useState('programming')
+    const [debouncedTerm,setDebouncedTerm]=useState(term)
     const [results,setResults]=useState([])
 
     useEffect(()=>{
@@ -18,7 +17,7 @@ const Search=()=>{
 
     useEffect(()=>{
         const search=async ()=>{
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+            const {data}=await axios.get('https://en.wikipedia.org/w/api.php',{
                 params:{
                     action:'query',
                     list:'search',
@@ -27,33 +26,23 @@ const Search=()=>{
                     srsearch:debouncedTerm
                 }
             })
-
             setResults(data.query.search)
         }
         search()
-        
     },[debouncedTerm])
 
     const renderedResults=results.map(result=>{
         return (
-            <div className="item" key={result.pageid}>
-                <div className="content">
-                    <div className="header">{result.title}</div>
-                    <span dangerouslySetInnerHTML={{__html:result.snippet}}></span>
-                </div>
-                <div className="right floated content">
-                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
-                </div>
+            <div key={result.pageid}>
+
             </div>
         )
     })
-
     return (
         <div>
-            <input className="input" value={term} onChange={(e)=>setTerm(e.target.value)}  />
-            <div>{renderedResults}</div>
+            <input onChange={e=>setTerm(e.target.value)} />
+            {renderedResults}
         </div>
-        
     )
 }
 
