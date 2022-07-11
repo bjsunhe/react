5eddf4f058a05873171971d1fe7d497b84759c3f
bjsunhe <bjsunhe@sina.com>
2022-07-11 09:42:41 +0800

    curry

---
+++ print/curry.js
@@ -0,0 +1,26 @@
+// function currying(fn,arr=[]){
+    
+//     return function(...args){
+//         arr=[...arr,...args]
+//         if(arr.length<fn.length){
+//             return currying(fn,arr)
+//         }else{
+//             return fn(...arr)
+//         }
+//     }
+// }
+
+
+function currying(fn){
+    let args=[]
+    const inner=(arr=[])=>{
+        args.push(...arr)
+        return args.length>=fn.length?fn(...args):(...args)=>inner(args)
+    }
+    return inner()
+}
+function add(a,b,c,d){
+    return a+b+c+d
+}
+
+let curriedAdd=currying(add)(1)(2)(3)(4)
